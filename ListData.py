2a31f80718af6e050cb75398be5bb4fc7079a8f7
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 08:46:29 +0700

    menambahkan append dan extend function pada ListData.py

--- ListData.py
+++ ListData.py
@@ -5,6 +5,19 @@
 
 
 list1 = [1, 2, 3]
-list2 = [1, Raihan, True] #list bisa menyimpan berbagai macam tipe data
+list2 = [1, 1.3 , "Raihan", True] #list bisa menyimpan berbagai macam tipe data
+
+#Untuk menambahkan data ke list terdapat 3 method yaitu 
+#append, extend, insert
+
+#append adalah method untuk menambahkan elemen data baru ke bagian akhir list
+list1.append("Budi") # menambahkan string baru ke list1
+print(list1)
+
+#extend method berfungsi untuk menambahkan list atau data struktur satu ke data struktur lainnya
+list1.extend(list2) # extends akan menambahkan list2 ke list1 pada bagian akhir list1
+print(list1)
+
+
 
 
