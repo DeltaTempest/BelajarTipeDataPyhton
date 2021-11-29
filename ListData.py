ec4641c648c7a8611e4929b41139d8e850145045
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 09:32:36 +0700

    menambahkan remove function dan cara mengakses list

--- ListData.py
+++ ListData.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def showHeader(namaFunction):
-    print("======== ", namaFunction , " Function ========\n")
+    print("======== ", namaFunction , " ========\n")
 
 #List adalah struktur data pada pyhton yang bisa menyimpan berbagai macam tipe data.
 #list juga dapat merubah tipe data pada data yang ada di struktur tersebut.
@@ -13,20 +13,25 @@ def showHeader(namaFunction):
 list1 = [1, 2, 3]
 list2 = [1, 1.3 , "Raihan", True] #list bisa menyimpan berbagai macam tipe data
 
+#Untuk mengaksesnya bisa dengan cara berikut
+print(list1) #printnya secara langsung secara keseluruhan
+print(list1[0]) #print index nya secara spesifik
+print(list1[0:2]) #print dari index ke 0 sampai 2
+
 #Untuk menambahkan data ke list terdapat 3 method yaitu 
 #append, extend, insert
 
-showHeader("Append")
+showHeader("Append Function")
 #append function berfungsi untuk menambahkan elemen data baru ke bagian akhir list
 list1.append("Budi") # menambahkan string baru ke list1
 print(list1, "\n")
 
-showHeader("Extend")
+showHeader("Extend Function")
 #extend function berfungsi untuk menambahkan list atau data struktur satu ke data struktur lainnya
 list1.extend(list2) # extends akan menambahkan list2 ke list1 pada bagian akhir list1
 print(list1, "\n")
 
-showHeader("Insert")
+showHeader("Insert Function")
 #insert function berfungsi untuk menambahkan elemen data ke list pada index atau lokasi yang spesifik
 list1.insert(0, "Index pertama")
 print(list1 , "\n")
@@ -38,14 +43,14 @@ listBaru = [1, 2 , 3, 4, 5]
 
 #del function berfungsi mendelet elemen data pada index tertentu, atau dari index 1 sampai ke n pada list
 #del function hanya berfungsi untuk menghapus data tidak mengembalikannya
-showHeader("Del")
+showHeader("Del Function")
 del listBaru[0] #menghapus elemen pada index ke 0
 print(listBaru , "\n")
 
 del listBaru[2:4] #menghapus elemen pada index ke n sampai ke n - 1
 print(listBaru, "\n")
 
-showHeader("Pop")
+showHeader("Pop Function")
 #Pop function berfungsi untuk menghapus atau mengembalikan elemen data pada list secara spesifik
 
 listNama = ["Raihan", "Awang", "Budi"]
@@ -53,7 +58,13 @@ print("Elemen yang dihilangkan:",listNama.pop(1), "\n") #dia akan mengembalikan 
 
 print("List nya sekarang",listNama, "\n")
 
-showHeader("Remove")
+showHeader("Remove Function")
+#Seperti namanya remove function berfungsi untuk menghapus elemen data yang spesifik pada list. Remove di tentukan bukan oleh index
+
+listBuah = ['mangga', 'apel', 'pisang']
+listBuah.remove('mangga') # menghapus elemen pisang pada listBuah
+print("ListBuah: ", listBuah, "\n")
+
 
 
 
