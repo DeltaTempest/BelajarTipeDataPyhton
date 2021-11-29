40387f7fb8cee7957506627590d2b162d9b9833a
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 10:31:59 +0700

    Menambahkan langkah membuat dictionary di DictDataTYpe.py

---
+++ DictDataType.py
@@ -0,0 +1,9 @@
+#Dictionary atai dict adalah data struktur yang memiliki key dan value
+#data pada dict tidak bisa dirubah rubah
+#cara penulisan
+
+dict1 = {"Tempest" : "Suka makan apel",
+         "Rimuru" : "Adalah slime"
+         }
+#Disini Tempest, dan Rimuru adalah key, dan "suka makan apel" dan "Adalah slime" adalah value
+
