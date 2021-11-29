ea55ed3b722f564226c03e03eece69b64b2982f8
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 16:37:28 +0700

    menambahkan add function pada setsdatatype

--- SetsDataType.py
+++ SetsDataType.py
@@ -1,3 +1,18 @@
-#sets adalah koleksi atau data struktur yang tidak teratur yang terdiri dari berbagai macam elemen unik
+from ListFungsi import *
+
+#sets adalah koleksi atau data struktur yang tidak teratur, yang terdiri dari berbagai macam elemen unik, dan datanya tidak bisa dirubah
+#tidak teratur berarti kita tidak bisa mengakses secara index spesifik terhadap value sets
 #sets syntax
 #NamaVariabel = {masukan elemen}
+
+sets1 = {1, 2, 3, 4, "herwin"}
+
+showHeader("Mengakses sets")
+#sets hanya bisa diakses secara keseluruhan tidak bisa satu persatu
+print(sets1) #print sets1
+
+showHeader("Menambahkan elemen baru")
+#untuk menambahkan elemen baru pada sets bisa 
+sets1.add
+
+
