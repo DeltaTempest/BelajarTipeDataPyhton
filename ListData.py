94b6d547cc7971cf4173b7a3c86102f29ed12fb4
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 08:58:26 +0700

    menambahkan insert function pada ListData.py

--- ListData.py
+++ ListData.py
@@ -10,12 +10,16 @@ list2 = [1, 1.3 , "Raihan", True] #list bisa menyimpan berbagai macam tipe data
 #Untuk menambahkan data ke list terdapat 3 method yaitu 
 #append, extend, insert
 
-#append adalah method untuk menambahkan elemen data baru ke bagian akhir list
+#append function berfungsi untuk menambahkan elemen data baru ke bagian akhir list
 list1.append("Budi") # menambahkan string baru ke list1
 print(list1)
 
-#extend method berfungsi untuk menambahkan list atau data struktur satu ke data struktur lainnya
+#extend function berfungsi untuk menambahkan list atau data struktur satu ke data struktur lainnya
 list1.extend(list2) # extends akan menambahkan list2 ke list1 pada bagian akhir list1
+print(list1)
+
+#insert function berfungsi untuk menambahkan elemen data ke list pada index atau lokasi yang spesifik
+list1.insert(0, "Index pertama")
 print(list1)
 
 
