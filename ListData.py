7788ede1691443a2933edf998bec991f3b00238f
DeltaTempest <raihan5.herwin@gmail.com>
2021-11-29 08:33:08 +0700

    Membuat file ListData.py

---
+++ ListData.py
@@ -0,0 +1,10 @@
+#List adalah struktur data pada pyhton yang bisa menyimpan berbagai macam tipe data.
+#list juga dapat merubah tipe data pada data yang ada di struktur tersebut.
+#cara membuat list adalah dengan cara berikut
+#nama variabel = [isi data]
+
+
+list1 = [1, 2, 3]
+list2 = [1, Raihan, True] #list bisa menyimpan berbagai macam tipe data
+
+
