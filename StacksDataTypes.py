ffe0a0357943ca32ed4d277b0b0d831c04c13165
DeltaTempest <raihan5.herwin@gmail.com>
2021-12-01 13:25:07 +0700

    Menambahkan Pengertian stack pada StacskDataTypes.py

--- StacksDataTypes.py
+++ StacksDataTypes.py
@@ -3,4 +3,4 @@
 #stack mempunyai pointer yang namanya TOP dimana fungsinya untuk melacak element yang ada di paling atas
 #cara kerja stack adalah saat data ditambahkan, TOP pointer akan merujuk pada data yang masuk
 #lalu saat ada data yang dimasukan kembali, data tersebut akan disimpan diatas data sebelumnya dan TOP pointer akan naik ke data baru
-#saat kita mengeluarkan data paling atas, TOP pointer akan turun dan data paling atas akan keluar
+#saat kita mengeluarkan data paling atas, TOP pointer akan turun dan data paling atas akan keluar dari stack
