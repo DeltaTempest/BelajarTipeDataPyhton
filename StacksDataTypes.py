8b2fccc44b8f3f38ae1b4d14846a16470108dcd4
DeltaTempest <raihan5.herwin@gmail.com>
2021-12-01 13:23:57 +0700

    Menghapus beberapa lines

---
+++ StacksDataTypes.py
@@ -0,0 +1,6 @@
+#Stack adalah data struktur yang terbuat dari Array
+#stack mempunyai aturan LIFO atau (Last In First Out) dimana yang terakhir masuk akan pertama kali keluarnya
+#stack mempunyai pointer yang namanya TOP dimana fungsinya untuk melacak element yang ada di paling atas
+#cara kerja stack adalah saat data ditambahkan, TOP pointer akan merujuk pada data yang masuk
+#lalu saat ada data yang dimasukan kembali, data tersebut akan disimpan diatas data sebelumnya dan TOP pointer akan naik ke data baru
+#saat kita mengeluarkan data paling atas, TOP pointer akan turun dan data paling atas akan keluar
